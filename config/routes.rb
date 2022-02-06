1f7349dd67413f6b93c6e2a45e46e3c179bcf38e
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-02-06 18:22:03 +0100

    Create a route for the root page

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "splash#index"
 
   resources :categories, only: [:index, :show] do
     resources :records, only: [:index, :show]
   end
-
-  
 end
