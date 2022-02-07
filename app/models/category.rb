4891479a0f82e1f275cd87bd8b65823bcd3388d8
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-02-07 12:25:14 +0100

    Validate the parameters for category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,7 @@ class Category < ApplicationRecord
   belongs_to :user
   has_many :categories_records
   has_many :records, through: :categories_records
+
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :icon, presence: true, length: { maximum: 250 }
 end
