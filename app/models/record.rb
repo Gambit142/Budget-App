3f9828de1fabfc476ef313b187fbcc930b1b1b16
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-02-07 13:22:25 +0100

    Add validation tests for Recor Model

--- app/models/record.rb
+++ app/models/record.rb
@@ -3,6 +3,6 @@ class Record < ApplicationRecord
   has_many :categories_records
   has_many :categories, through: :categories_records
 
-  validates :name, presence: true, length: { maximum: 250 }
+  validates :name, presence: true, length: { maximum: 100 }
   validates :amount, presence: true, numericality: { greater_than: 0 }
 end
--- spec/models/record_spec.rb
+++ spec/models/record_spec.rb
@@ -1,8 +1,36 @@
-require 'rails_helpers'
+require 'rails_helper'
 
 describe Record, type: :model do
   before(:each) do
     @user = User.create!(name: 'Leroy', email: 'leroy@gmail.com', password: '123456')
-    @record = @user.categories.create!(name: 'Games', icon: 'PlayStation 5')
+    @record = @user.records.create!(name: 'Nintendo Switch', amount: 50)
+  end
+
+  it 'Record should be valid' do
+    expect(@record).to be_valid
+  end
+
+  it 'Record should not be valid without a name' do
+    @record.name = nil
+    expect(@record).to_not be_valid
+    @record.name = ''
+    expect(@record).to_not be_valid
+  end
+
+  it 'Record should not be valid if name is too long' do
+    @record.name = 'g' * 101
+    expect(@record).to_not be_valid
+  end
+
+  it 'Record should not be valid if amount is negative' do
+    @record.amount = -2
+    expect(@record).to_not be_valid
+  end
+
+  it 'Record should not be valid if amount is not a float' do
+    @record.amount = 'fifty'
+    expect(@record).to_not be_valid
+    @record.amount = false
+    expect(@record).to_not be_valid
   end
 end
