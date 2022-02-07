893074a7fa5532488611db9ab40f08064fcb65d6
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-02-07 12:25:51 +0100

    Validate the parameters for Record model

--- app/models/record.rb
+++ app/models/record.rb
@@ -2,4 +2,7 @@ class Record < ApplicationRecord
   belongs_to :user
   has_many :categories_records
   has_many :categories, through: :categories_records
+
+  validates :name, presence: true, length: { maximum: 250 }
+  validates :amount, presence: true, numericality: { greater_than: 0 }
 end
