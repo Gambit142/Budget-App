1bdfa3d93be2b939d95b1b189df600e614d6bb21
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-02-09 19:00:01 +0100

    Add a method that ensures the user is directed to the Home Page when logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  # end
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || categories_path
+  end
 
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |u|
