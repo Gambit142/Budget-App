6cc0b6ffb9ce2ae4aed5d9b1a0fc661c0c0c7135
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-02-10 08:29:41 +0100

    Write tests to make active storage pass

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,34 +3,32 @@ require 'rails_helper'
 describe Category, type: :model do
   before(:each) do
     @user = User.create!(name: 'Leroy', email: 'leroy@gmail.com', password: '123456')
-    @category = @user.categories.create!(name: 'Games', icon: 'PlayStation 5')
+    @category = @user.categories.new(name: 'Games')
+    @category.icon.attach(
+      io: File.open(Rails.root.join('spec', 'fixtures', 'multi-colored.png')), 
+      filename: 'multi-colored.png',
+      content_type: 'application/xlsx'
+    )
+    @category.save!
   end
 
-  it 'Category should be valid' do
+  it 'should be valid' do
     expect(@category).to be_valid
   end
 
-  it 'Category should not be valid without a name' do
+  it 'should not be valid without a name' do
     @category.name = nil
     expect(@category).to_not be_valid
     @category.name = ''
     expect(@category).to_not be_valid
   end
 
-  it 'Category should not be valid if name is too long' do
+  it 'should not be valid if name is too long' do
     @category.name = 'g' * 101
     expect(@category).to_not be_valid
   end
 
-  it 'Category should not be valid without an icon' do
-    @category.icon = nil
-    expect(@category).to_not be_valid
-    @category.icon = ''
-    expect(@category).to_not be_valid
-  end
-
-  it 'Category should not be valid if icon is too long' do
-    @category.icon = 'g' * 251
-    expect(@category).to_not be_valid
+  it 'should have a file attached' do
+    expect(@category.icon).to be_attached
   end
 end
